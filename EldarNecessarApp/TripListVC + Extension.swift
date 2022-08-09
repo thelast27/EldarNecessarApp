e5535caac84bdd379502fe4d43dd75b24ca9c7f5
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-09 16:06:59 +0300

    add table view extesnion

--- EldarNecessarApp/TripListVC + Extension.swift
+++ EldarNecessarApp/TripListVC + Extension.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TripTableViewCell", for: indexPath) as? TripTableViewCell else { return UITableViewCell() }
+        cell.textLabel?.text = "Hello"
+        return UITableViewCell()
+    }
+    
+    
+}
