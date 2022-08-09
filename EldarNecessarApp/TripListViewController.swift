75f482c8adaa1d5a5292afd97f52ace71b142781
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-09 16:07:49 +0300

    reg cell and add button for main VC

--- EldarNecessarApp/TripListViewController.swift
+++ EldarNecessarApp/TripListViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class TripListViewController: UIViewController {
 
+    @IBOutlet weak var tripListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tripListTableView.register(UINib(nibName: "TripTableViewCell", bundle: nil), forCellReuseIdentifier: "TripTableViewCell")
 
-        // Do any additional setup after loading the view.
+ 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addNewTrip(_ sender: Any) {
+    
+        tripListTableView.reloadData()
     }
-    */
+    
 
 }
