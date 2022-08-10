f6a0e750aee4b47501aa84fd2f292345f4980f52
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-10 10:32:18 +0300

    set no nil checking for text field

--- EldarNecessarApp/Viewcontrollers/SetTripViewController.swift
+++ EldarNecessarApp/Viewcontrollers/SetTripViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class SetTripViewController: UIViewController {
 
+    @IBOutlet weak var tripName: UITextField!
+    @IBOutlet weak var tripNotes: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveSetTrip(_ sender: Any) {
+        guard tripName.hasText,
+        let textForCell = tripName.text
+        else { return }
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
