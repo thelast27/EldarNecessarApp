1cfadf52a028ef2845db9cb4f947c61910cb355a
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-10 09:56:09 +0300

    add files for setting trip page

---
+++ EldarNecessarApp/SetTripViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SetTripViewController.swift
+//  EldarNecessarApp
+//
+//  Created by Eldar Garbuzov on 10.08.22.
+//
+
+import UIKit
+
+class SetTripViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
