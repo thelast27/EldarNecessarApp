282d48ccc18aadf20554a8fbfc2a10a5d9d8f5bb
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-11 17:25:45 +0300

    add comment about bug

--- EldarNecessarApp/Extensions/TripListVC + Extension.swift
+++ EldarNecessarApp/Extensions/TripListVC + Extension.swift
@@ -18,7 +18,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
 
         if let cell = tableView.dequeueReusableCell(withIdentifier: TripTableViewCell.key, for: indexPath) as? TripTableViewCell {
             cell.tripName.text = tripsArray[indexPath.row]
-            cell.tripPhoto.image = UIImage(contentsOfFile: (contentOfDerectory?[indexPath.row].path) ?? "person")
+            cell.tripPhoto.image = UIImage(contentsOfFile: (contentOfDerectory?[indexPath.row].path) ?? "person") // если элементов (строк в данном случае) больше, чем картинок для них, то выскакивает ошибка Thread 1: Fatal error: Index out of range
             return cell
         }
         return UITableViewCell()
