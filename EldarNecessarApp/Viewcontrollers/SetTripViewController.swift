f7d566afe5402b3ee734443498ac13a2586e645b
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-11 17:26:11 +0300

    add ing picker and file manager for pics

--- EldarNecessarApp/Viewcontrollers/SetTripViewController.swift
+++ EldarNecessarApp/Viewcontrollers/SetTripViewController.swift
@@ -9,26 +9,43 @@ import UIKit
 
 class SetTripViewController: UIViewController {
     
-
-
+    let fileManager = FileManager.default
+    
     @IBOutlet weak var tripName: UITextField!
     @IBOutlet weak var tripNotes: UITextField!
+    @IBOutlet weak var previewOfTripPhoto: UIImageView!
     
+    var documentsURL: URL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        contentOfDerectory = try? fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: .none)
+        
     }
     
-
+    
     @IBAction func saveSetTrip(_ sender: Any) {
         guard tripName.hasText,
-        let textForCell = tripName.text
+              let textForCell = tripName.text
         else { return }
         addItem(name: textForCell)
         dismiss(animated: true) {
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "tableViewChanged"), object: nil)
-
+            
         }
     }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true)
+    }
 }
+
+
+
+
