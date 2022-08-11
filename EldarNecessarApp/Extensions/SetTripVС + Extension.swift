ad56cda543b4b15d0d3fff19a3f0ad8d30ca4c67
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-11 17:25:30 +0300

    set extension for img picker

--- EldarNecessarApp/Extensions/SetTripVС + Extension.swift
+++ EldarNecessarApp/Extensions/SetTripVС + Extension.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+extension SetTripViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let imageURL = info[.imageURL] as? URL,
+           let editedImage = info[.editedImage] as? UIImage {
+            let newImageURL = documentsURL.appendingPathComponent(imageURL .lastPathComponent)
+            let imageJpgData = editedImage.jpegData(compressionQuality: 1)
+            do {
+                try imageJpgData?.write(to: newImageURL)
+                contentOfDerectory?.append(newImageURL)
+                previewOfTripPhoto.image = editedImage
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "tableViewChanged"), object: nil)
+            } catch {
+                print(error)
+            }
+            dismiss(animated: true)
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
