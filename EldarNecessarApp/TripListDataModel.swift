5dee2d6b1736ff062dfe1a8316eadab8653d7fd8
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-12 14:41:55 +0300

    del code from model file

--- EldarNecessarApp/TripListDataModel.swift
+++ EldarNecessarApp/TripListDataModel.swift
@@ -8,21 +8,5 @@
 import Foundation
 import UIKit
 
-var tripsArray: [String] = []
-var contentOfDerectory: [URL]!
 
 
-func addItem(name: String) {
-    tripsArray.append(name)
-    saveItem()
-}
-
-
-func removeItem(at index: Int ) {
-    tripsArray.remove(at: index)
-    saveItem()
-}
-
-func saveItem() {
-    
-}
