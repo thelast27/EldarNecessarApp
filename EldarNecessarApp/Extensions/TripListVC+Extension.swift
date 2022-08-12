854fb959504a45a8f8aea6c373dfafa616c01a6c
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-12 14:45:38 +0300

    fix comment with bug and fix variables foe table view extension

--- EldarNecessarApp/Extensions/TripListVC+Extension.swift
+++ EldarNecessarApp/Extensions/TripListVC+Extension.swift
@@ -11,14 +11,14 @@ import UIKit
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tripsArray.count
+        return arrayWithTrips.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         if let cell = tableView.dequeueReusableCell(withIdentifier: TripTableViewCell.key, for: indexPath) as? TripTableViewCell {
-            cell.tripName.text = tripsArray[indexPath.row]
-            cell.tripPhoto.image = UIImage(contentsOfFile: (contentOfDerectory?[indexPath.row].path) ?? "person") // если элементов (строк в данном случае) больше, чем картинок для них, то выскакивает ошибка Thread 1: Fatal error: Index out of range
+            cell.tripName.text = arrayWithTrips[indexPath.row]
+            cell.tripPhoto.image = UIImage(contentsOfFile: (contentOfDerectory[indexPath.row].path)) // если пытаюсь загрузить просто текст, без фото - ошибка Thread 1: Fatal error: Index out of range
             return cell
         }
         return UITableViewCell()
