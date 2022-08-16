071bac32d0f06f391ef3e9514e5916f10cf07d67
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-16 16:47:44 +0300

    add file for item cell in item list

---
+++ EldarNecessarApp/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  EldarNecessarApp
+//
+//  Created by Eldar Garbuzov on 16.08.22.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
