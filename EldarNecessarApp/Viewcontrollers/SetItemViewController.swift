0ddc3df89c4800b86a54b984525cdf4a97ee6f85
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-16 13:30:05 +0300

    add stepper for items qty and add picker view for category choosing

--- EldarNecessarApp/Viewcontrollers/SetItemViewController.swift
+++ EldarNecessarApp/Viewcontrollers/SetItemViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class SetItemViewController: UIViewController {
-
+    
+    
+    let categoryPickerView = UIPickerView()
+    
+    @IBOutlet weak var itemQtyLabel: UILabel!
+    @IBOutlet weak var qtyItemsStepper: UIStepper!
+    @IBOutlet weak var categoryTextField: UITextField!
+    
+    
+    var categoryPickerArray: [String] = ["Outdoor", "Clothing", "Comfort & Entertaiment", "Documents", "Electronic & Gadget", "Family", "Medical & Health", "Toiletries", "Others"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        qtyItemsStepper.wraps = false
+        qtyItemsStepper.autorepeat = true
+        categoryPickerView.delegate = self
+        categoryPickerView.dataSource = self
+        
+        categoryTextField.inputView = categoryPickerView
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func qtyItemsStepperAction(_ sender: UIStepper) {
+        itemQtyLabel.text = Int(sender.value).description
     }
-    */
+}
 
+extension SetItemViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryPickerArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryPickerArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryTextField.text = categoryPickerArray[row]
+        categoryTextField.resignFirstResponder()
+    }
+    
 }
