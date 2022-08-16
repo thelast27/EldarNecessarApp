a1da272240edec76b1f9f5ef4efa39fce63bc287
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-16 12:49:42 +0300

    add new bar button item programmatically and set move from one vc to other

--- EldarNecessarApp/Viewcontrollers/ItemsViewController.swift
+++ EldarNecessarApp/Viewcontrollers/ItemsViewController.swift
@@ -12,18 +12,16 @@ class ItemsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemAction))
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addItemAction(_ sender: Any) {
+        let viewController = UIStoryboard(name: "ItemsList", bundle: nil)
+        if let VC = viewController.instantiateViewController(withIdentifier: "SetItemVC") as? SetItemViewController {
+        let navigationVC = UINavigationController(rootViewController: VC)
+        present(navigationVC, animated: true)
     }
-    */
-
+     
+}
 }
