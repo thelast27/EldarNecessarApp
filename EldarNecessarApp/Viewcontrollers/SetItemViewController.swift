a199c28f9cabd06367930cb2f52435c92862f6ba
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-19 15:04:45 +0300

    remove picker extension from separate file to related VC and make array with default categories to private
    also del empty file

--- EldarNecessarApp/Viewcontrollers/SetItemViewController.swift
+++ EldarNecessarApp/Viewcontrollers/SetItemViewController.swift
@@ -19,7 +19,7 @@ class SetItemViewController: UIViewController {
     @IBOutlet weak var itemDescrip: UITextField!
     
     
-    var categoryPickerArray: [String] = ["Outdoor", "Clothing", "Comfort & Entertaiment", "Documents", "Electronic & Gadget", "Family", "Medical & Health", "Toiletries", "Others"]
+    private var categoryPickerArray: [String] = ["Outdoor", "Clothing", "Comfort & Entertaiment", "Documents", "Electronic & Gadget", "Family", "Medical & Health", "Toiletries", "Others"]
     var otherItemDetails: [String] = []
     typealias SendItemName = (String) -> ()
     var itemsClosure: SendItemName?
@@ -65,4 +65,25 @@ class SetItemViewController: UIViewController {
     }
 }
 
+extension SetItemViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryPickerArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryPickerArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryTextField.text = categoryPickerArray[row]
+        categoryTextField.resignFirstResponder()
+    }
+    
+}
 
