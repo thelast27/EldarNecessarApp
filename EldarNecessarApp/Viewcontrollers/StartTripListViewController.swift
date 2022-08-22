526110f27fa5b4aaca1b629b14c08f5e16945963
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-22 12:22:17 +0300

    set reflection saved data in realm on table view

--- EldarNecessarApp/Viewcontrollers/StartTripListViewController.swift
+++ EldarNecessarApp/Viewcontrollers/StartTripListViewController.swift
@@ -6,29 +6,33 @@
 //
 
 import UIKit
+import RealmSwift
 
 class StartTripListViewController: UIViewController {
-    
-    
 
+    
     @IBOutlet weak var tripListTableView: UITableView!
     
+    let realm = try! Realm()
     
     var arrayWithTrips: [String] = []
     var contentOfDirectory: [URL] = []
+//    var trips: [Trips] = []
+    var resultsRealmData: Results<Trips>!
+    var vcWithRealm = SetTripViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tripListTableView.register(UINib(nibName: "TripTableViewCell", bundle: nil), forCellReuseIdentifier: TripTableViewCell.key)
+       resultsRealmData = vcWithRealm.getRealmData()
     }
-    
 
     
-    
+
     @IBAction func addTrip(_ sender: Any) {
         if let VC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SetTripViewController") as? SetTripViewController {
             VC.tripsClosure = { [ weak self ] text in
-                self?.arrayWithTrips.insert(text, at: 0)
+//                self?.trips.insert(text, at: 0)
                 self?.tripListTableView.reloadData()
             }
             VC.picsClosure = { [ weak self ] pics in
