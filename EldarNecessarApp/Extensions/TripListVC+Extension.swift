b0d83b1ef0a8a21ff213ac6d58cb597bf3bd0a87
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-22 12:21:08 +0300

    fix extension for save data via realm
    fix source of cell and add method for delete cells.

--- EldarNecessarApp/Extensions/TripListVC+Extension.swift
+++ EldarNecessarApp/Extensions/TripListVC+Extension.swift
@@ -11,21 +11,39 @@ import UIKit
 extension StartTripListViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayWithTrips.count
+        return resultsRealmData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: TripTableViewCell.key, for: indexPath) as? TripTableViewCell {
-            cell.tripName.text = arrayWithTrips[indexPath.row]
-            cell.tripPhoto.image = UIImage(contentsOfFile: (contentOfDirectory[indexPath.row].path)) // если пытаюсь загрузить просто текст, без фото - ошибка Thread 1: Fatal error: Index out of range
-            return cell
-        }
-        return UITableViewCell()
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: TripTableViewCell.key, for: indexPath) as? TripTableViewCell {
+//            cell.tripName.text = arrayWithTrips[indexPath.row]
+//            cell.tripPhoto.image = UIImage(contentsOfFile: (contentOfDirectory[indexPath.row].path)) // если пытаюсь загрузить просто текст, без фото - ошибка Thread 1: Fatal error: Index out of range
+//            return cell
+//        }
+//        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TripTableViewCell.key, for: indexPath) as? TripTableViewCell else { return UITableViewCell() }
+        let trip = resultsRealmData[indexPath.row]
+        cell.tripName.text = trip.tripName
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let vc = UIStoryboard(name: "ItemsList", bundle: nil).instantiateViewController(withIdentifier: "ItemsVC") as? ItemsViewController else { return }
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            if let item = resultsRealmData?[indexPath.row] {
+                try! realm.write {
+                    realm.delete(item)
+                }
+                tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+            }
+        }
+    }
+
 }
