895d3ab3fa97ff4ac825b97a6eba9214aaad8205
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-27 16:12:26 +0300

    set needed code (just for correct presentation of VC)

--- EldarNecessarApp/Viewcontrollers/EditItemViewController.swift
+++ EldarNecessarApp/Viewcontrollers/EditItemViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class EditItemViewController: UIViewController {
+    
+    let categoryPickerView = UIPickerView()
 
+    @IBOutlet weak var categoryTextField: UITextField!
+    
+    private var categoryPickerArray: [String] = ["Outdoor", "Clothing", "Comfort & Entertaiment", "Documents", "Electronic & Gadget", "Family", "Medical & Health", "Toiletries", "Others"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        categoryPickerView.delegate = self
+        categoryPickerView.dataSource = self
+        categoryTextField.inputView = categoryPickerView
         // Do any additional setup after loading the view.
     }
     
@@ -26,4 +34,24 @@ class EditItemViewController: UIViewController {
     }
     */
 
+}
+
+extension EditItemViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryPickerArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryPickerArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryTextField.text = categoryPickerArray[row]
+        categoryTextField.resignFirstResponder()
+    }
+    
 }
