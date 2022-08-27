fb10ae497f0a58a7a6ffcbde80b74a456b91f509
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-27 12:12:16 +0300

    add items list table view to relevant extension fie

--- EldarNecessarApp/Extensions/ItemsListVC+Extension.swift
+++ EldarNecessarApp/Extensions/ItemsListVC+Extension.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
+
+extension ItemsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultsRealmDataWithItem.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ItemTableViewCell.key, for: indexPath) as? ItemTableViewCell else { return UITableViewCell() }
+        let items = resultsRealmDataWithItem[indexPath.row]
+        cell.itemName.text = items.itemName
+        cell.itemDescription.text = items.itemDescription
+        cell.itemQty.text = "\(items.itemQty)"
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
