855a6d0b16678f1949e7ea34eeabe94688218d0b
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-30 19:38:57 +0300

    set example location for map VC

--- EldarNecessarApp/Viewcontrollers/MapViewController.swift
+++ EldarNecessarApp/Viewcontrollers/MapViewController.swift
@@ -9,34 +9,32 @@ import UIKit
 import GoogleMaps
 
 class MapViewController: UIViewController {
-
+    
+    private var realmManager = RealmManager()
+    var trips = Trips()
+    
+    
     @IBOutlet weak var viewForMap: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-                let mapView = GMSMapView.map(withFrame: self.viewForMap.frame, camera: camera)
-                self.viewForMap.addSubview(mapView)
-
-                // Creates a marker in the center of the map.
-                let marker = GMSMarker()
-                marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-                marker.title = "Sydney"
-                marker.snippet = "Australia"
-                marker.map = mapView
+        let mapView = GMSMapView.map(withFrame: self.viewForMap.frame, camera: camera)
+        self.viewForMap.addSubview(mapView)
+        
+        let title = trips.tripName
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//        marker.title = "Sydney"
+        marker.title = title
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
 
 extension MapViewController: GMSMapViewDelegate {
