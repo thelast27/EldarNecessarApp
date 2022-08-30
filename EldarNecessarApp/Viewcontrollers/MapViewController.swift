7eed9128fc4854773f437a3eb69750501c832a45
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-30 18:52:21 +0300

    add sample code with location and marker

--- EldarNecessarApp/Viewcontrollers/MapViewController.swift
+++ EldarNecessarApp/Viewcontrollers/MapViewController.swift
@@ -14,10 +14,16 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let camera = GMSCameraPosition.camera(withLatitude: 54.029, longitude: 27.597, zoom: 5.0)
-        let mapView = GMSMapView.map(withFrame: viewForMap.bounds, camera: camera)
-        mapView.delegate = self
-        viewForMap.addSubview(mapView)
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+                let mapView = GMSMapView.map(withFrame: self.viewForMap.frame, camera: camera)
+                self.viewForMap.addSubview(mapView)
+
+                // Creates a marker in the center of the map.
+                let marker = GMSMarker()
+                marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+                marker.title = "Sydney"
+                marker.snippet = "Australia"
+                marker.map = mapView
     }
     
 
