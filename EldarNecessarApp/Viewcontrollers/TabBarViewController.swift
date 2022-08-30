a13a72ec64f1b3a79ca602ed521fde86113fc78d
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-08-30 17:28:17 +0300

    add file for tabbar and add first VC inside

---
+++ EldarNecessarApp/Viewcontrollers/TabBarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarViewController.swift
+//  EldarNecessarApp
+//
+//  Created by Eldar Garbuzov on 30.08.22.
+//
+
+import Foundation
+import UIKit
+
+class TabBarViewControoller: UITabBarController {
+    
+    override func viewDidLoad() {
+        guard let weatherViewContoller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TripListViewController") as? StartTripListViewController else { return }
+        
+        viewControllers = [addViewController(viewController: UINavigationController(rootViewController: weatherViewContoller), title: "Trips", image: UIImage(systemName: "airplane.departure"))]
+    }
+    
+    
+    private func addViewController(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+        viewController.tabBarItem.title = title
+        viewController.tabBarItem.image = image
+        return viewController
+        
+    }
+    
+}
