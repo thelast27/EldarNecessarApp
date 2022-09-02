5e21d725cb52f14bfe10654a01ab44c65aadaa53
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-02 12:26:44 +0300

    set map vc

--- EldarNecessarApp/Viewcontrollers/MapViewController.swift
+++ EldarNecessarApp/Viewcontrollers/MapViewController.swift
@@ -7,40 +7,52 @@
 
 import UIKit
 import GoogleMaps
+import RealmSwift
 
 class MapViewController: UIViewController {
     
-    private var realmManager = RealmManager()
-    var trips = Trips()
-    
-    
     @IBOutlet weak var viewForMap: UIView!
+    @IBOutlet weak var tripPlaceSelected: UIButton!
+    
+    private var realmManager = RealmManager()
+    var trips: Trips!
+    var long: Double = 0.0
+    var lat: Double = 0.0
+    var id: ObjectId!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(closeScreen))
         
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 54.029, longitude: 27.597, zoom: 5.0)
         let mapView = GMSMapView.map(withFrame: self.viewForMap.frame, camera: camera)
+        mapView.delegate = self
         self.viewForMap.addSubview(mapView)
-        
-        let title = trips.tripName
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//        marker.title = "Sydney"
-        marker.title = title
-        marker.snippet = "Australia"
-        marker.map = mapView
+        trips = realmManager.realm.object(ofType: Trips.self, forPrimaryKey: id)
     }
     
     @objc func closeScreen() {
         dismiss(animated: true)
     }
-
-    
+    @IBAction func savePlaceForTRip(_ sender: Any) {
+        realmManager.addMarkerToRealm(lat: lat, long: long, in: trips) {
+            dismiss(animated: true)
+        }
+    }
 }
 
 extension MapViewController: GMSMapViewDelegate {
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        marker.map = mapView
+        tripPlaceSelected.isEnabled = true
+        lat = coordinate.latitude
+        long = coordinate.longitude
+
+    }
     
 }
