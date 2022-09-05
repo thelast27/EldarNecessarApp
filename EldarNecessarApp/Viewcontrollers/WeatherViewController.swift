7543c36cf16dd5e63617213eb0748dde3bc9b0eb
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-05 12:18:32 +0300

    add cell on VC and set extension

--- EldarNecessarApp/Viewcontrollers/WeatherViewController.swift
+++ EldarNecessarApp/Viewcontrollers/WeatherViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        weatherTableView.register(UINib(nibName: "CurrentWeatherTableViewCell", bundle: nil), forCellReuseIdentifier: "CurrentWeatherCell")
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+}
+
+extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentWeatherCell", for: indexPath) as? CurrentWeatherTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    
+ 
+    
+    
 }
