64436afca7f147533bb60fd018d854f44bfa62de
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-05 18:03:39 +0300

    set weather manager

--- EldarNecessarApp/Manager/WeatherManager.swift
+++ EldarNecessarApp/Manager/WeatherManager.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+enum Units {
+    case celsius
+    var main: String {
+        switch self {
+        case .celsius: return "metric"
+        }
+    }
+}
+
+protocol APIProviderProtocol {
+    func getWeatherForCityCoordinates(long: Double, lat: Double, withUnitsOfmeasurement units: Units, completionHandler: @escaping (CurrentAndForecastWeather) -> Void)
+}
+
+class WeatherManager: APIProviderProtocol {
+    
+    var weatherApiKey: String {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "weatherApiKey") as? String else { return "" }
+        return key
+    }
+    
+    func getWeatherForCityCoordinates(long: Double, lat: Double, withUnitsOfmeasurement units: Units, completionHandler: @escaping (CurrentAndForecastWeather) -> Void) {
+        let endpoint = Endpoint.currentWeather(lat: lat, lon: long, key: weatherApiKey, units: units.main)
+        var urlRequest = URLRequest(url: endpoint.url )
+        urlRequest.httpMethod = "GET"
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                print(error)
+            }
+            if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let currentWeather = try decoder.decode(CurrentAndForecastWeather.self, from: data)
+                    completionHandler(currentWeather)
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+}
+
+
