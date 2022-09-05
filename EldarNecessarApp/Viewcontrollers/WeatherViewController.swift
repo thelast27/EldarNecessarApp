91e34e2bf1135489a95b343c1390b38dd19170ef
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-05 18:04:28 +0300

    set weather vc

--- EldarNecessarApp/Viewcontrollers/WeatherViewController.swift
+++ EldarNecessarApp/Viewcontrollers/WeatherViewController.swift
@@ -11,9 +11,23 @@ class WeatherViewController: UIViewController {
 
     @IBOutlet weak var weatherTableView: UITableView!
     
+    var lat: Double = 0.0
+    var long: Double = 0.0
+    var currentAndForecustedWeather: CurrentAndForecastWeather?
+    var weatherManagerDelegte: APIProviderProtocol = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         weatherTableView.register(UINib(nibName: "CurrentWeatherTableViewCell", bundle: nil), forCellReuseIdentifier: "CurrentWeatherCell")
+        
+        weatherManagerDelegte.getWeatherForCityCoordinates(long: long, lat: lat, withUnitsOfmeasurement: .celsius) { [weak self] weatherData in
+            guard let self = self else { return }
+            self.currentAndForecustedWeather = weatherData
+            DispatchQueue.main.async {
+                self.weatherTableView.reloadData()
+            }
+        }
+        
     }
     
 
@@ -23,15 +37,25 @@ class WeatherViewController: UIViewController {
 
 extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentWeatherCell", for: indexPath) as? CurrentWeatherTableViewCell else { return UITableViewCell() }
+        if let current = currentAndForecustedWeather {
+            cell.update(date: current)
+        }
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        250
+    }
     
  
     
