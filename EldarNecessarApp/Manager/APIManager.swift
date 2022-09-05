45c61618945ed1b1900326a30abb8ab9395541cb
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-05 18:03:13 +0300

    add api communication with weather service

--- EldarNecessarApp/Manager/APIManager.swift
+++ EldarNecessarApp/Manager/APIManager.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+enum Endpoint {
+    case getIcon(icon: String)
+    case currentWeather(lat: Double, lon: Double, key: String, units: String)
+}
+
+extension Endpoint {
+    var weatherURL: String {
+        return "https://api.openweathermap.org/"
+    }
+    
+    var iconURL: String {
+        return "https://openweathermap.org/"
+    }
+    
+    var url: URL {
+        switch self {
+            
+            case .getIcon(let icon):
+            if let url = URL(string: iconURL.appending("img/wn/\(icon)@2x.png")) {
+                return url
+            }
+            fatalError()
+            
+        case .currentWeather(let lat, let lon, let key, let units):
+            if let url = URL(string: weatherURL.appending("data/2.5/onecall?lat=\(lat)&lon=\(lon)&appid=\(key)&units=\(units)")) {
+                return url
+            }
+            fatalError()
+        }
+    }
+    
+    
+}
+
