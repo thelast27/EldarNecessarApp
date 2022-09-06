424bb59841bf669d9d75da83ed82a9761e0a420f
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-06 15:21:37 +0300

    reset API manager

--- EldarNecessarApp/Manager/APIManager.swift
+++ EldarNecessarApp/Manager/APIManager.swift
@@ -3,13 +3,14 @@
 //  EldarNecessarApp
 //
 //  Created by Eldar Garbuzov on 5.09.22.
-//
+
 
 import Foundation
 
 enum Endpoint {
+    case geocodingURL(key: String, city: String)
     case getIcon(icon: String)
-    case currentWeather(lat: Double, lon: Double, key: String, units: String)
+    case currentWeather(lat: Double, lon: Double, key: String, lang: String, units: String)
 }
 
 extension Endpoint {
@@ -23,15 +24,18 @@ extension Endpoint {
     
     var url: URL {
         switch self {
-            
-            case .getIcon(let icon):
+        case .geocodingURL(let key, let city):
+            if let url = URL(string: weatherURL.appending("geo/1.0/direct?q=\(city)&appid=\(key)")) {
+                return url
+            }
+            fatalError()
+        case .getIcon(let icon):
             if let url = URL(string: iconURL.appending("img/wn/\(icon)@2x.png")) {
                 return url
             }
             fatalError()
-            
-        case .currentWeather(let lat, let lon, let key, let units):
-            if let url = URL(string: weatherURL.appending("data/2.5/onecall?lat=\(lat)&lon=\(lon)&appid=\(key)&units=\(units)")) {
+        case .currentWeather(let lat, let lon, let key, let lang, let units):
+            if let url = URL(string: weatherURL.appending("data/2.5/onecall?lat=\(lat)&lon=\(lon)&appid=\(key)&lang=\(lang)&units=\(units)")) {
                 return url
             }
             fatalError()
