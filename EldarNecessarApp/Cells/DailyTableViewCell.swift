b387eef2428eaaef44286a8cb17073a1da052798
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-06 15:21:09 +0300

    set daily table view cell

--- EldarNecessarApp/Cells/DailyTableViewCell.swift
+++ EldarNecessarApp/Cells/DailyTableViewCell.swift
@@ -8,16 +8,64 @@
 import UIKit
 
 class DailyTableViewCell: UITableViewCell {
+    
+    var dailyWeather: [DailyWeatherData]?
+    
 
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+//        self.selectionStyle = .none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
+    func update(date: DailyWeatherData) {
+        guard let dt = date.dt, let temp = date.temp?.day, let icon = date.weather?.first?.icon else { return }
+        let endpoint = Endpoint.getIcon(icon: "\(icon)")
+        DispatchQueue.global(qos: .utility).async {
+            guard let iconData = try? Data(contentsOf: endpoint.url) else { return }
+            DispatchQueue.main.async {
+                self.weatherImage.image = UIImage(data: iconData)
+            }
+        }
+        dayLabel.text = dt.updateDateFormat(dateFormat: .days)
+        tempLabel.text = "\(Int(temp)) °C"
+        
+    }
+    
+}
+
+
+extension Int {
+    func updateDateFormat(dateFormat: DateFormat) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self))
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.dateFormat = dateFormat.getDateFormat
+        dateFormatter.timeZone = .current
+        return dateFormatter.string(from: date)
+    }
+}
+
+enum DateFormat: String {
+    case hours
+    case days
+    case fullTime
+    case daysWithoutYear
+    
+    var getDateFormat: String {
+        switch self {
+        case .hours: return "HH"
+        case .days: return "E"
+        case .daysWithoutYear: return "HH MMMM yyyy"
+        case .fullTime: return "EEEE, d MMMM yyyy HH:mm:ss"
+        }
+    }
 }
