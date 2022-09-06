8f73ca6858f85af37491d88558661c67fde20958
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-06 15:55:20 +0300

    remove table view extension from weather vc

--- EldarNecessarApp/Viewcontrollers/WeatherViewController.swift
+++ EldarNecessarApp/Viewcontrollers/WeatherViewController.swift
@@ -28,60 +28,13 @@ class WeatherViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         weatherManagerDelegte.getWeatherForCityCoordinates(long: long, lat: lat, withLang: .english, withUnitsOfmeasurement: .celsius) { [weak self] weatherData in
             guard let self = self else { return }
-                     self.currentAndForecustedWeather = weatherData
-                     self.dailyWeather = weatherData.daily
-                     DispatchQueue.main.async {
-                         self.weatherTableView.reloadData()
-                     }
+            self.currentAndForecustedWeather = weatherData
+            self.dailyWeather = weatherData.daily
+            DispatchQueue.main.async {
+                self.weatherTableView.reloadData()
+            }
         }
-//        weatherManagerDelegte.getCoordinatesByName(forCity: "Dublin") { [weak self] weatherData in
-//            guard let self = self else { return }
-//            self.currentAndForecustedWeather = weatherData
-//            self.dailyWeather = weatherData.daily
-//            DispatchQueue.main.async {
-//                self.weatherTableView.reloadData()
-//            }
-//        }
     }
     
 }
 
-extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        2
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else {
-            return dailyWeather?.count ?? 2
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentWeatherCell", for: indexPath) as? CurrentWeatherTableViewCell else { return UITableViewCell() }
-            if let current = currentAndForecustedWeather {
-                cell.update(date: current)
-            }
-            return cell
-        } else if indexPath.section == 1 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "DailyWeather", for: indexPath) as? DailyTableViewCell else { return UITableViewCell() }
-            if let daily = dailyWeather {
-                cell.update(date: daily[indexPath.row])
-            }
-            return cell
-        }
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return 250
-        } else {
-            return 70
-        }
-    }
-}
