f43907b6d81e99b61a7dbbc8fd9a4bfb8cf44124
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-06 18:11:15 +0300

    change color on clear

--- EldarNecessarApp/Viewcontrollers/StartTripListViewController.swift
+++ EldarNecessarApp/Viewcontrollers/StartTripListViewController.swift
@@ -12,6 +12,7 @@ class StartTripListViewController: UIViewController {
     
     
     @IBOutlet weak var tripListTableView: UITableView!
+    @IBOutlet weak var backgroundPic: UIImageView!
     
     
     var contentOfDirectory: [URL] = []
@@ -20,11 +21,13 @@ class StartTripListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.white
+        backgroundPic.image = UIImage(named: "tripsListVC")
+
         self.navigationItem.title = "Trips list"
+
         self.navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTripAction))
+        navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = .black
         tripListTableView.register(UINib(nibName: "TripTableViewCell", bundle: nil), forCellReuseIdentifier: TripTableViewCell.key)
         resultsRealmData = realmManager.getTripDataFromRealm()
     }
