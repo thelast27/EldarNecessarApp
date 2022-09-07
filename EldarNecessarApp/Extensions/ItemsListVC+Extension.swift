8e32949ea349ceae2cede152229e0ab897a32e2c
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-07 12:11:33 +0300

    fix if on ternary expressions

--- EldarNecessarApp/Extensions/ItemsListVC+Extension.swift
+++ EldarNecessarApp/Extensions/ItemsListVC+Extension.swift
@@ -33,39 +33,27 @@ extension ItemsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         guard let vc = UIStoryboard(name: "ItemsList", bundle: nil).instantiateViewController(withIdentifier: "EditItemVC") as? EditItemViewController, let trip = trips else { return }
-                    vc.titleForTop = resultsRealmDataWithItem[indexPath.row].itemName
+        vc.titleForTop = resultsRealmDataWithItem[indexPath.row].itemName
         vc.items = resultsRealmDataWithItem[indexPath.row]
         vc.trips = trip
-                    navigationController?.pushViewController(vc, animated: true)
+        navigationController?.pushViewController(vc, animated: true)
         
     }
     
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let items = resultsRealmDataWithItem[indexPath.row]
-        if items.isPacked == false {
-            let doCheckMark = UIContextualAction(style: .normal, title: "Check") { [weak self] (action, view, completionHandler) in
-                
-                guard let items = self?.resultsRealmDataWithItem[indexPath.row], let trip = self?.trips else { return }
-                self?.realmManager.writeItemIsPackedDataToRealm(item: items, in: trip)
-                tableView.reloadData()
-                completionHandler(true)
-            }
-            doCheckMark.backgroundColor = .systemGreen
+        let doCheckMark = UIContextualAction(style: .normal, title: "\(items.isPacked == false ? "Check" : "Uncheck")") { [weak self] (action, view, completionHandler) in
             
-            return UISwipeActionsConfiguration(actions: [doCheckMark])
-            
-        } else {
-            let doUnCheckMark = UIContextualAction(style: .normal,  title: "Unheck") { [weak self] (action, view, completionHandler) in
-                
-                guard let items = self?.resultsRealmDataWithItem[indexPath.row] else { return }
-                self?.realmManager.writeItemIsPackedDataToRealm(item: items, in: self?.trips ?? Trips.init())
-                tableView.reloadData()
-                completionHandler(true)
-            }
-            doUnCheckMark.backgroundColor = .systemBlue
-            
-            return UISwipeActionsConfiguration(actions: [doUnCheckMark])  }
+            guard let items = self?.resultsRealmDataWithItem[indexPath.row], let trip = self?.trips else { return }
+            self?.realmManager.writeItemIsPackedDataToRealm(item: items, in: trip)
+            tableView.reloadData()
+            completionHandler(true)
+        }
+        
+        doCheckMark.backgroundColor = items.isPacked == false ? .systemGreen : .systemBlue
+        
+        return UISwipeActionsConfiguration(actions: [doCheckMark])
     }
 }
 
