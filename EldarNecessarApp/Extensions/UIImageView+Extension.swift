9c5bdbcf2f43a6e35e8fa5bb6e6cbdadef58ffb1
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-09-07 16:37:02 +0300

    add UIImageView extension

--- EldarNecessarApp/Extensions/UIImageView+Extension.swift
+++ EldarNecessarApp/Extensions/UIImageView+Extension.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIImageView {
+    func getImageFromTheInternet(_ icon: String) {
+        let endpoint = Endpoint.getIcon(icon: icon)
+        var icon: Data?
+        DispatchQueue.global(qos: .utility).async {
+            guard let iconData = try? Data(contentsOf: endpoint.url) else { return }
+            icon = iconData
+            DispatchQueue.main.async {
+                guard let icon = icon else { return }
+                self.image = UIImage(data: icon)
+            }
+        }
+    }
+}
